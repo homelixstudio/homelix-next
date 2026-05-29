ab35c909fdf6dad8a309465c34e6c95f1d4e4dab
Rakesh <homelixstudio@gmail.com>
2026-05-29 22:36:33 +0530

    Add robots and sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,44 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://homelixstudio.in",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://homelixstudio.in/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://homelixstudio.in/services",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://homelixstudio.in/our-work",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://homelixstudio.in/living-room",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://homelixstudio.in/bedroom",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://homelixstudio.in/kitchen",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://homelixstudio.in/dining",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://homelixstudio.in/office",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://homelixstudio.in/wardrobe",
+      lastModified: new Date(),
+    },
+  ];
+}
